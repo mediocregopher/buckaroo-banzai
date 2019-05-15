9f1f9232cc5af31a865782188968785edc0af2c2
Brian Picciano <mediocregopher@gmail.com>
2019-05-14 21:16:33 -0600

    add federation server endpoint

--- go.mod
+++ go.mod
@@ -3,16 +3,38 @@ module buckaroo-bonzai
 go 1.12
 
 require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/gavv/monotime v0.0.0-20190418164738-30dba4353424 // indirect
+	github.com/go-errors/errors v1.0.1 // indirect
 	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/imkira/go-interpol v1.1.0 // indirect
+	github.com/jarcoal/httpmock v1.0.4 // indirect
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/lusis/go-slackbot v0.0.0-20180109053408-401027ccfef5 // indirect
 	github.com/lusis/slack-test v0.0.0-20190426140909-c40012f20018 // indirect
+	github.com/manucorporat/sse v0.0.0-20160126180136-ee05b128a739 // indirect
+	github.com/mattn/go-colorable v0.1.1 // indirect
 	github.com/mediocregopher/mediocre-go-lib v0.0.0-20190430005813-a07df614d896
 	github.com/mediocregopher/radix/v3 v3.2.3
+	github.com/moul/http2curl v1.0.0 // indirect
 	github.com/nlopes/slack v0.5.0
 	github.com/nullstyle/go-xdr v0.0.0-20180726165426-f4c839f75077 // indirect
 	github.com/onsi/ginkgo v1.8.0 // indirect
 	github.com/onsi/gomega v1.5.0 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/segmentio/go-loggly v0.5.0 // indirect
+	github.com/sirupsen/logrus v1.4.1 // indirect
+	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a // indirect
 	github.com/stellar/go v0.0.0-20190430222051-cc129a7f0599
 	github.com/stellar/go-xdr v0.0.0-20180917104419-0bc96f33a18e // indirect
+	github.com/valyala/fasthttp v1.2.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.1.0 // indirect
+	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 // indirect
+	github.com/yudai/gojsondiff v1.0.0 // indirect
+	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
+	github.com/yudai/pp v2.0.1+incompatible // indirect
+	gopkg.in/gavv/httpexpect.v1 v1.0.0 // indirect
 )
--- stellar.go
+++ stellar.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
+	"strings"
 	"text/template"
 
 	"github.com/mediocregopher/mediocre-go-lib/mcfg"
@@ -11,13 +13,18 @@ import (
 	"github.com/mediocregopher/mediocre-go-lib/mhttp"
 	"github.com/mediocregopher/mediocre-go-lib/mlog"
 	"github.com/mediocregopher/mediocre-go-lib/mrun"
+	"github.com/stellar/go/clients/horizonclient"
 	"github.com/stellar/go/keypair"
 	"github.com/stellar/go/strkey"
 )
 
+const federationPath = "/api/federation"
+
 type stellar struct {
-	ctx context.Context
-	kp  *keypair.Full
+	ctx    context.Context
+	kp     *keypair.Full
+	domain string
+	client *horizonclient.Client
 
 	*http.ServeMux
 }
@@ -29,9 +36,17 @@ func withStellar(parent context.Context) (context.Context, *stellar) {
 	}
 
 	s.ServeMux.Handle("/.well-known/stellar.toml", http.HandlerFunc(s.tomlHandler))
+	s.ServeMux.Handle(federationPath, http.HandlerFunc(s.federationHandler))
 
-	var seedStr *string
+	var (
+		seedStr *string
+		domain  *string
+		live    *bool
+	)
 	s.ctx, seedStr = mcfg.WithRequiredString(s.ctx, "seed", "Seed for account which will issue CRYPTICBUCKs")
+	s.ctx, domain = mcfg.WithRequiredString(s.ctx, "domain", "Domain the server will be served from")
+	s.ctx, live = mcfg.WithBool(s.ctx, "live", "Use the live network.")
+
 	s.ctx = mrun.WithStartHook(s.ctx, func(context.Context) error {
 		seedB, err := strkey.Decode(strkey.VersionByteSeed, *seedStr)
 		if err != nil {
@@ -48,6 +63,15 @@ func withStellar(parent context.Context) (context.Context, *stellar) {
 		s.kp = pair
 		s.ctx = mctx.Annotate(s.ctx, "address", s.kp.Address())
 		mlog.Info("loaded stellar seed", s.ctx)
+
+		s.domain = *domain
+		s.ctx = mctx.Annotate(s.ctx, "domain", s.domain)
+
+		if *live {
+			s.client = horizonclient.DefaultPublicNetClient
+		} else {
+			s.client = horizonclient.DefaultTestNetClient
+		}
 		return nil
 	})
 
@@ -58,6 +82,7 @@ func withStellar(parent context.Context) (context.Context, *stellar) {
 
 var stellarTOMLTPL = template.Must(template.New("").Parse(`
 ACCOUNTS=["{{.Address}}"]
+FEDERATION_SERVER="{{.FederationAddr}}"
 
 [[CURRENCIES]]
 CODE="CRYPTICBUCK"
@@ -73,10 +98,37 @@ func (s *stellar) tomlHandler(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Set("Access-Control-Allow-Origin", "*")
 	rw.Header().Set("Content-Type", "text/toml")
 
-	err := stellarTOMLTPL.Execute(rw, struct{ Address string }{
-		Address: s.kp.Address(),
+	err := stellarTOMLTPL.Execute(rw, struct{ Address, FederationAddr string }{
+		Address:        s.kp.Address(),
+		FederationAddr: s.domain + federationPath,
 	})
 	if err != nil {
 		mlog.Error("error executing toml template", s.ctx, merr.Context(err))
 	}
+}
+
+const notFoundStr = `{"detail":"not found"}`
+
+func (s *stellar) federationHandler(rw http.ResponseWriter, r *http.Request) {
+	if r.FormValue("type") != "name" {
+		http.Error(rw, notFoundStr, 404)
+		return
+	}
+
+	q := r.FormValue("q")
+	if !strings.HasSuffix(q, "*"+s.domain) {
+		http.Error(rw, notFoundStr, 404)
+		return
+	}
+
+	// We don't want to actually check if the username is a member of the slack
+	// channel and return based on that, because someone would be able to use
+	// that to enumerate all the users of a group. So just always return the
+	// address, if someone sends money to the wrong account then.... thanks!
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	rw.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(map[string]string{
+		"stellar_address": q,
+		"account_id":      s.kp.Address(),
+	})
 }
